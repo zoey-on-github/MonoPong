153020bce564042778d31f0038bba7e77d7fa575
zoey-on-github <88806187+zoey-on-github@users.noreply.github.com>
2024-07-29 00:59:17 -0700

    yeah

---
+++ Game1.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MonoPong;
+
+
+public class Game1 : Game
+{
+
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+   static  byte redValue = 193;
+    static byte greenValue = 105;
+    static byte blueValue = 79;
+     Color realBrown = new Color(redValue, greenValue, blueValue);
+        public Texture2D pixel;
+
+       public Rectangle leftPaddle;
+    public Game1()
+    {
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+        // A single-pixel texture
+    }
+
+    protected override void Initialize()
+    {
+        // TODO: Add your initialization logic here
+
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+        pixel = new Texture2D(GraphicsDevice, 1, 1);
+        pixel.SetData<Color>(new Color[]
+        {
+            Color.White
+        });
+        leftPaddle = new Rectangle(100, 100, 30, 100);
+
+        // TODO: use this.Content to load your game content here
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+        // TODO: Add your update logic here
+
+        base.Update(gameTime);
+    }
+    protected override void Draw(GameTime gameTime)
+    {
+        _spriteBatch.Begin();
+        GraphicsDevice.Clear(Color.Black);
+        _spriteBatch.Draw(pixel, leftPaddle, realBrown);
+        _spriteBatch.End();
+
+        // TODO: Add your drawing code here
+
+        base.Draw(gameTime);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new MonoPong.Game1();
+game.Run();
